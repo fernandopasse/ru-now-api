395cca9057dc4725a557489dc28bfb07b4502bcd
Luandro <luandro@gmail.com>
2016-05-02 20:49:14 -0300

    added config file, and resolved warnings

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongodb: 'ufvjs:vicosa2016@ds013202.mlab.com:13202/runow',
+  clientID: '521375034735921',
+  clientSecret: '4bc315eba8c63a2f91f36c7192b337b4',
+  facebookCallback: 'http://localhost:8001/auth/facebook/callback',
+  facebookSuccess: 'http://locahost:3000/',
+  facebookFail: 'http://locahost:3000/',
+  logout: 'http://locahost:3000/',
+  sessionSecret: 'monkey ballz'
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// config
+import config from '../config';
 // Express app & Socket io
 import express from 'express';
 import cookieParser from 'cookie-parser';
@@ -12,17 +14,22 @@ import passport from 'passport';
 const FacebookStrategy = require('passport-facebook').Strategy;
 // MongoDB
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://ufvjs:vicosa2016@ds013202.mlab.com:13202/runow');
+mongoose.connect(`mongodb://${config.mongodb}`);
 const User = mongoose.model('User', {name: String, email: String, username: String, provider: String, facebook: {name: String, id: String}});
 
 // Express config
 app.use(cookieParser());
-app.use(bodyParser());
-app.use(session({ secret: 'keyboard cat' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: config.sessionSecret,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(express.static(__dirname + '/public'));
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(app.router);
+
 passport.serializeUser((user, done) => {
     done(null, user.id);
     // if you use Model.id as your idAttribute maybe you'd want
@@ -36,33 +43,29 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(new FacebookStrategy({
-    clientID: '521375034735921',
-    clientSecret: '4bc315eba8c63a2f91f36c7192b337b4',
-    callbackURL: "http://localhost:8001/auth/facebook/callback"
+    clientID: config.clientID,
+    clientSecret: config.clientSecret,
+    callbackURL: config.facebookCallback
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
-    User.findOne({
-      'facebook.id': profile.id
-    }, (err, user) => {
+    User.findOne({ 'facebook.id': profile.id }, (err, user) => {
       if (err) {
           return done(err);
       }
     //No user was found... so create a new user with values from Facebook (all the profile. stuff)
       if (!user) {
-          user = new User({
-              name: profile.displayName,
-              // email: profile.emails[0].value,
-              username: profile.username,
-              provider: 'facebook',
-              //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
-              facebook: profile._json
-          });
-          user.save((err) => {
-            console.log("ERROR FACEBOOK LOGIN");
-            if (err) console.log(err);
-            return done(err, user);
-          });
+        user = new User({
+          name: profile.displayName,
+          username: profile.username,
+          provider: 'facebook',
+          //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+          facebook: profile._json
+        });
+        user.save((err) => {
+          console.log("ERROR FACEBOOK SAVE");
+          if (err) console.log(err);
+          return done(err, user);
+        });
       } else {
         //found user. Return
         if(err) {
@@ -77,12 +80,17 @@ passport.use(new FacebookStrategy({
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/login' }));
-app.get('/', (req, res) => {
-  res.send('SUCESSSSOOOOO')
-})
+app.get('/auth/facebook/callback',passport.authenticate('facebook', {
+  successRedirect: config.facebookSuccess,
+  failureRedirect: config.facebookFail
+}));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect(config.logout);
+});
+
+
 io.on('connection', (socket) => {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', (data) => {
